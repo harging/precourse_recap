5d677cd006285db3546f723662cbceee6e4ee351
Gavin Hargin <gavin.hargin@gmail.com>
2021-10-18 16:45:10 +0100

    guessing game

---
+++ precourse_recap.py
@@ -0,0 +1,8 @@
+input("What number between 1 and 5 am I thinking of? ")
+
+number = int(input)
+
+if number == 3:
+    print("Correct! Are you psychic?")
+else:
+    print("No, that's not it. You are wrong")
